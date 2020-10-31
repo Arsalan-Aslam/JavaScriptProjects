e65741fa82b0964518a6a82d503d63880d038c33
Arsalan-Aslam <arsalanaslam89@gmail.com>
2020-10-31 14:53:13 +0500

    Update script.js
    added functionality to check email validation, check length validation, and also password match validation

--- p01/script.js
+++ p01/script.js
@@ -5,6 +5,7 @@ const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
 // All functions
+
 // Function to show error
 function showError(input, message) {
     const formControl = input.parentElement;
@@ -19,11 +20,14 @@ function showSuccess(input) {
 }
 
 // Function to check if email is valid
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if ( re.test(input.value.trim()) ) {
+        showSuccess(input);
+    } else {
+        showError(input,`Please provide a valid email`);
+    }
 }
-
 // Function to check is required fields have data
 function checkRequired(inputArray) {
     inputArray.forEach(function(input) {
@@ -33,6 +37,24 @@ function checkRequired(inputArray) {
             showSuccess(input);
         }
     });
+}
+
+// Function to check length pf input field
+function checkLength(input,min,max) {
+    if ( input.value.length < min ) {
+        showError(input,`${getFieldId(input)} needs to be at least ${min} characters`);
+    } else if ( input.value.length > max) {
+        showError(input,`${getFieldId(input)} needs to be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+// Function to check if password and confirm password match
+function checkPasswordsMatch(input1, input2) {
+    if ( input1.value !== input2.value) {
+        showError(input2,"Passwords don't match");
+    }
 }
 
 // Function to get the id of the input field with proper case
@@ -45,4 +67,9 @@ form.addEventListener('submit',function(e) {
     e.preventDefault();
     
     checkRequired([username,email,password,password2]);
+    checkLength(username,3,10);
+    checkLength(password,6,30);
+    checkEmail(email);
+    checkPasswordsMatch(password,password2);
+
 })
